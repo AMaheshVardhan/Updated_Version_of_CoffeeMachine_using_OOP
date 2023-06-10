a3bd047f33aa0050a0cbcba376917703934c6c49
Mahesh-Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-06-10 21:02:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+coffee_maker.report()
+money_machine.report()
+while is_on:
+    options = menu.get_items()
+    choice = input(f"Enter the drink need to be done {options}: ")
+    if choice == "off":
+        print("Now machine is under maintenance mode")
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
